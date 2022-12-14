f538b4cae532ced206515c092f4819056e808463
Сергей Соболев <serg2010sob@gmail.com>
2022-12-14 22:30:17 +0300

    addlogic

--- Stocks/Stocks/settings.py
+++ Stocks/Stocks/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Stocks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Stocks\\StocksApp\\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Stocks/StocksApp/views.py
+++ Stocks/StocksApp/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import TickerForm
 
 def index(request):
-    return HttpResponse('Hi')
+    if request.method == 'POST':
+        form = TickerForm(request.POST)
+        if form.is_valid():
+            ticker = request.POST['ticker']
+            return HttpResponseRedirect(ticker)
+    else:
+        form = TickerForm()
+    return render(request, 'index.html',{'form':form})
+
+
+def ticker(request, id):
+    context = {}
+    context['ticker'] = id
+    return render(request, 'ticker.html', context)
